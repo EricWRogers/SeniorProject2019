78028ee8ccd3788551eff61333651107455d7da4
Brandon Demello <demellobrandon@gmail.com>
2019-01-12 13:02:27 -0600

    Ai Fixed and Updated

---
+++ Assets/Scripts/AI/Controllers/EntityStats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu (menuName ="PluggableAI/EntityStats")]
+public class EntityStats : ScriptableObject
+{
+    public float moveSpeed = 10;
+    public float lookRange = 40f;
+    public float lookSphereCastRadius = 1f;
+
+    //attack variables
+    public int attackRange = 5;
+    public float attackRate = 2f;
+
+    public float searchDuration = 4f;
+    public float searchingTurnSpeed = 2f;
+
+
+
+
+}
