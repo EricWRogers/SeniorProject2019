1e6c2a30a38ad4892064f0b4b451c614532d1d11
Ryan Buech <ryanbuech@gmail.com>
2019-01-22 12:44:14 -0600

    Creates Main Menu

---
+++ Assets/Scripts/UI/CreditMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditMovement : MonoBehaviour
+{
+    public float speed = 0f;
+    public float startYPos = -470.0f;
+    private Vector3 pos;
+
+    void Start()
+    {
+        pos = transform.position;
+        pos.y = startYPos;
+        transform.position = pos;
+    }
+
+    void Update()
+    {
+        pos.y += speed / 100;
+        transform.position = pos;
+    }
+
+    public void ResetPosition()
+    {
+        pos = transform.position;
+        pos.y = startYPos;
+        transform.position = pos;
+    }
+}
