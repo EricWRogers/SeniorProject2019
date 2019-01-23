db444b9889060e8a8700a3a6b3ba840c64baadf5
Ryan Buech <ryanbuech@gmail.com>
2019-01-22 19:30:40 -0600

    Adds Pause Screen

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenuUI;
+    public GameObject optionsMenuUI;
+    public GameObject creditsUI;
+    public string gameSceneName;
+    public float creditDuration = 100f;
+
+    public void Play()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void LoadOptions()
+    {
+        mainMenuUI.SetActive(false);
+        optionsMenuUI.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        optionsMenuUI.SetActive(false);
+        mainMenuUI.SetActive(true);
+    }
+
+    public void LoadCredits()
+    {
+        mainMenuUI.SetActive(false);
+        creditsUI.SetActive(true);
+        Invoke("CloseCredits", creditDuration);
+    }
+
+    public void CloseCredits()
+    {
+        creditsUI.SetActive(false);
+        mainMenuUI.SetActive(true);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public GameObject optionsMenuUI;
+    public string mainMenuScene;
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    public void LoadOptions()
+    {
+        pauseMenuUI.SetActive(false);
+        optionsMenuUI.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        optionsMenuUI.SetActive(false);
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void Exit()
+    {
+        SceneManager.LoadScene(mainMenuScene);
+    }
+
+    void Update()
+    {
+       CheckPause();
+    }
+
+    void CheckPause()
+    {
+        if (InputManager.instance.Pause())
+        {
+            if (pauseMenuUI.activeSelf)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+}
