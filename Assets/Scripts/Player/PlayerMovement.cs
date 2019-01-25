3bf770b5834331325284934df6ecfe1eb7cca6f5
Justin Liles <Justin.Liles@Live.com>
2019-01-24 18:24:11 -0600

    Player Update

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,7 +10,7 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector3 moveDirection = Vector3.zero;
     private CharacterController controller;
-    public float sprintMeater = 100.0f;
+    public float sprintMeater = 50.0f;
 
     void Start()
     {
@@ -22,10 +22,6 @@ public class PlayerMovement : MonoBehaviour
     {
         if (controller.isGrounded)
         {
-            moveDirection = new Vector3(InputManager.instance.Move().x, 0.0f, InputManager.instance.Move().z);
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection = moveDirection * speed;
-
             if (InputManager.instance.Sprint())
             {
                 if(sprintMeater <= 100.0f && sprintMeater >= 0.0f)
@@ -48,7 +44,7 @@ public class PlayerMovement : MonoBehaviour
             {
                 speed = originalSpeed;
 
-                if(sprintMeater > 100.0f)
+                if(sprintMeater >= 100.0f)
                 {
                     sprintMeater = 100.0f;
                 }
@@ -58,6 +54,9 @@ public class PlayerMovement : MonoBehaviour
                 }
             }
 
+            moveDirection = new Vector3(InputManager.instance.Move().x, 0.0f, InputManager.instance.Move().z);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * speed;
         }
 
         moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
