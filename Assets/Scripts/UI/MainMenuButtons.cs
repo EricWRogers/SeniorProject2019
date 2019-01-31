8406d7d3acd61d9b68ecef064a28daa87e5caa02
Ryan Buech <ryanbuech@gmail.com>
2019-01-31 01:34:29 -0600

    Connects sliders to audio mixer

--- Assets/Scripts/UI/MainMenuButtons.cs
+++ Assets/Scripts/UI/MainMenuButtons.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuButtons : MonoBehaviour
 {
@@ -10,6 +11,17 @@ public class MainMenuButtons : MonoBehaviour
     public GameObject creditsUI;
     public string gameSceneName;
     public float creditDuration = 100f;
+
+    public Slider MasterVolumeSlider;
+    public Slider MusicVolumeSlider;
+    public Slider SFXVolumeSlider;
+
+    void Start()
+    {
+        MasterVolumeSlider.value = PlayerPrefs.GetFloat("MasterVolume", 0.75f);
+        MusicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+        SFXVolumeSlider.value = PlayerPrefs.GetFloat("SFXVolume", 0.75f);
+    }
 
     public void Play()
     {
