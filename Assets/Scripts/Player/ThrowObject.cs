997b4e7e48f4585e7e7cb9283cf7017198ec54ed
Justin Liles <Justin.Liles@Live.com>
2019-02-07 17:14:15 -0600

    throwable objects

--- Assets/Scripts/Player/ThrowObject.cs
+++ Assets/Scripts/Player/ThrowObject.cs
@@ -4,66 +4,43 @@ using UnityEngine;
 
 public class ThrowObject : MonoBehaviour
 {
-    public Transform player;
-    public Transform playerCam;
-    public float throwForce = 100;
-    bool hasPlayer = false;
-    bool beingCarried = false;
-    public int dmg;
-    private bool touched = false;
+    public GameObject player;
+    public GameObject hand;
+    public float throwForce;
+    bool PlayerHolding = false;
 
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+        hand = GameObject.Find("/Player/Hand");
+
+        PlayerHolding = false;
+        GetComponent<Rigidbody>().isKinematic = false;
 
     }
 
     void Update()
     {
-        float dist = Vector3.Distance(gameObject.transform.position, player.position);
-        if (dist <= 2.5f)
-        {
-            hasPlayer = true;
-        }
-        else
-        {
-            hasPlayer = false;
-        }
-        if (hasPlayer && InputManager.instance.ThrowObject())
+        float dist = Vector3.Distance(transform.position, player.transform.position);
+
+        if (dist <= 2.5f && Input.GetKeyDown(KeyCode.C))
         {
             GetComponent<Rigidbody>().isKinematic = true;
-            transform.parent = playerCam;
-            beingCarried = true;
+            PlayerHolding = true;
+            GetComponent<Collider>().enabled = !GetComponent<Collider>().enabled;
         }
-        if (beingCarried)
-        {
-            if (touched)
-            {
-                GetComponent<Rigidbody>().isKinematic = false;
-                transform.parent = null;
-                beingCarried = false;
-                touched = false;
-            }
-            if (Input.GetMouseButtonDown(0))
-            {
-                GetComponent<Rigidbody>().isKinematic = false;
-                transform.parent = null;
-                beingCarried = false;
-                GetComponent<Rigidbody>().AddForce(playerCam.forward * throwForce);
-            }
-            else if (Input.GetMouseButtonDown(1))
-            {
-                GetComponent<Rigidbody>().isKinematic = false;
-                transform.parent = null;
-                beingCarried = false;
-            }
-        }
-    }
 
-    void OnTriggerEnter()
-    {
-        if (beingCarried)
+        if (PlayerHolding)
         {
-            touched = true;
+            transform.position = hand.transform.position;
+        }
+
+        if (PlayerHolding && Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Collider>().enabled = !GetComponent<Collider>().enabled;
+            GetComponent<Rigidbody>().isKinematic = false;
+            GetComponent<Rigidbody>().AddForce(player.transform.forward * throwForce);
+            PlayerHolding = false;
         }
     }
 }
