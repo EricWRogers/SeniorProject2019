3eeb0bbf3aca41c66d13b60822043ffc7b5dc8ef
EricWRogers <yerick147083@gmail.com>
2019-03-05 17:10:43 -0600

    doors

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DoorScript : MonoBehaviour
 {
     Animator anim;
-
+    public string MyLock =  "";
 
     void Start()
     {
@@ -15,10 +15,16 @@ public class DoorScript : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("entity"))
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if(other.gameObject.GetComponent<KeyChain>().KeysInPocket.Contains(MyLock))
+            {
+                anim.SetBool("PlayerNearDoor", true);
+            }
+        }
+        if ( other.gameObject.CompareTag("entity"))
         {
             anim.SetBool("PlayerNearDoor", true);
-
         }
     }
     void OnTriggerExit(Collider other)
