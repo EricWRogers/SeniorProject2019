0f113bc60e5c4a9654d8119fb055a8cbb9084e14
Ryan Buech <ryanbuech@gmail.com>
2019-04-02 21:38:18 -0500

    Add random sounds to creature and music to game

---
+++ Assets/Scripts/Audio/RandomSoundPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSoundPlayer : MonoBehaviour
+{
+    public AudioClip[] audioClips;
+    public AudioSource audioSource;
+
+    float randomTime = 30;
+    float timeCounter = 0;
+    int randomSound = 0;
+    
+    void Update()
+    {
+        if(timeCounter > randomTime)
+        {
+            randomTime = Random.Range(10, 90);
+            timeCounter = 0;
+            audioSource.Stop();
+            ChooseSound();
+            audioSource.Play();
+        }
+
+        timeCounter += Time.deltaTime;
+    }
+
+    void ChooseSound()
+    {
+        randomSound = Random.Range(0, audioClips.Length);
+        audioSource.clip = audioClips[randomSound];
+    }
+}
